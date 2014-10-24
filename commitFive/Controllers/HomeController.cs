fa5251be56e7670b6dcd5f13b37e0fbf66ad1d89
robert <incephalon@gmail.com>
2014-10-24 10:08:20 -0500

    first commit

---
+++ commitFive/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace commitFive.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        private commit_fiveEntities db = new commit_fiveEntities();
+        
+
+        public ActionResult Index()
+        {
+            //student -> 
+
+
+            var x = db.campusReference.Where(c => c.CAMPUS=="'057905017")
+                .Select(c => new {  
+                    //campus = c.CAMPNAME,
+                    //staar = c.college.Where(c.college.)
+
+                });
+
+
+            return View(db.campusReference.ToList());
+            //return View(db.campusReference.ToList());
+        }
+    }
+}
---
+++ commitFive/Models/MyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace commitFive.Models
+{
+    public class MyViewModel
+    {
+        public int CPETALLC { get; set; }
+
+
+
+    }
+}
